fbb8d25ff6b0b14c6a2832398ed950c599613a80
Brooke Zhou <71456822+brookezhou@users.noreply.github.com>
2021-05-05 12:56:15 -0400

    tragic

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "f8c359893525fec83d43904a9eda906d";
+//const key = "f8c359893525fec83d43904a9eda906d";
 const query = async function () {
   try {
     const response = await fetch(
-      `http://api.weatherstack.com/current
-        ? access_key = f8c359893525fec83d43904a9eda906d
-        & query = New York`
+      `http://api.weatherstack.com/current?access_key=f8c359893525fec83d43904a9eda906d&units=f&query=${searchParams}`
     );
   } catch (error) {
     console.log(error);
