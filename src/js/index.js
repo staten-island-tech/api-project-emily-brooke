0fb9b3be9bc122bf2cf2f25e100ec0d71cb6aa8d
Brooke Zhou <71456822+brookezhou@users.noreply.github.com>
2021-05-04 21:44:11 -0400

    no entiendo part 2

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,15 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "f8c359893525fec83d43904a9eda906d";
+const query = async function () {
+  try {
+    const response = await fetch(
+      `http://api.weatherstack.com/current
+        ? access_key = f8c359893525fec83d43904a9eda906d
+        & query = New York`
+    );
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
